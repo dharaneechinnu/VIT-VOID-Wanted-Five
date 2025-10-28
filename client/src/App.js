34b9f3fdc8112a2f7e008e6c48b9f01b3172bb78
varsh-007 <varshini262005@gmail.com>
2025-10-28 18:35:59 +0530

    edited student details

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,29 @@ import './App.css';
 import VerifierLanding from './Verifier/VerifierLanding';
 import VerifierRegister from './Verifier/VerifierRegister';
 import VerifierLogin from './Verifier/VerifierLogin';
+import StudentLogin from './student/StudentLogin';
+import StudentRegister from './student/StudentRegister';
+
 
 function App() {
   return (
     <Router>
       <Routes>
         {/* 🌐 Verifier Landing Page */}
-        <Route path="/Verifier" element={<VerifierLanding />} />
+        <Route path="/" element={<VerifierLanding />} />
 
         {/* 🔑 Verifier Login */}
         <Route path="/Verifier/Login" element={<VerifierLogin />} />
 
         {/* 📝 Verifier Register */}
         <Route path="/Verifier/Register" element={<VerifierRegister />} />
+
+        {/* 🎓 Student Login */}
+        <Route path="/Student/Login" element={<StudentLogin />} />
+
+        {/* Student Register*/}
+        <Route path="/Student/Register" element={<StudentRegister />} />
+
       </Routes>
     </Router>
   );
